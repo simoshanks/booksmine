da1f9240e4fec38e760c997c06c1f92f10e01978
simoshanks <siomoazicode@gmail.com>
2025-03-07 15:18:11 +0100

    final de html

---
+++ script.js
@@ -0,0 +1,32 @@
+let books = [
+    {
+        title: "Book 1",
+        description: "Author 1",
+        image: ""
+    }, 
+    {
+        title: "Book 1",
+        description: "Author 1",
+        image: ""
+    }, 
+    {
+        title: "Book 1",
+        description: "Author 1",
+        image: ""
+    }, 
+    {
+        title: "Book 1",
+        description: "Author 1",
+        image: ""
+    }, 
+    {
+        title: "Book 1",
+        description: "Author 1",
+        image: ""
+    },
+
+]
+
+books.forEach(book => {
+    
+})
